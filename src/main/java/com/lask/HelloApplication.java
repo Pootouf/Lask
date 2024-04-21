0f3d62096881e1c77070a247208cc8ce881d9bd0
ellexart <axelle.mazuy@gmail.com>
2024-04-21 12:37:34 +0200

    feature: editing of priority column

--- src/main/java/com/lask/HelloApplication.java
+++ src/main/java/com/lask/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1500, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/lask/controller/TaskVisualizationController.java
+++ src/main/java/com/lask/controller/TaskVisualizationController.java
@@ -5,19 +5,21 @@ import com.lask.model.task.std.Priority;
 import com.lask.model.task.std.StdBasicTask;
 import com.lask.view.DateEditingCell;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.control.cell.TextFieldTreeTableCell;
-import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import javafx.scene.control.cell.*;
+import javafx.util.StringConverter;
+import javafx.util.converter.DefaultStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -39,6 +41,7 @@ public class TaskVisualizationController implements Initializable {
         this.createTreeViewTaskColumnIntegerFormat("Durée", "duration");
         this.createTreeViewTaskColumnIntegerFormat("Pourcentage de complétion", "completionPercentage");
         this.createTreeViewTaskColumnDateFormat("Date de fin", "endDate");
+        this.createTreeViewTaskColumnEnumPriorityFormat("Priorité", "priority");
     }
 
     private void createTreeViewTaskColumn(String columnName, String propertyName) {
@@ -61,4 +64,37 @@ public class TaskVisualizationController implements Initializable {
         column.setCellFactory(TextFieldTreeTableCell.forTreeTableColumn(new IntegerStringConverter()));
         treeView.getColumns().add(column);
     }
+
+    private void createTreeViewTaskColumnEnumPriorityFormat(String columnName, String propertyName) {
+        TreeTableColumn<Task, Priority> column = new TreeTableColumn<>(columnName);
+        column.setCellValueFactory(new TreeItemPropertyValueFactory<>(propertyName));
+        column.setCellFactory(col -> {
+            ComboBoxTreeTableCell<Task, Priority> tc = new ComboBoxTreeTableCell<Task, Priority>(
+                    new StringConverter<Priority>() {
+                        @Override
+                        public String toString(Priority priority) {
+                            return switch (priority) {
+                                case NORMAL -> "NORMAL";
+                                case SECONDARY -> "SECONDAIRE";
+                                case URGENT -> "URGENT";
+                            };
+                        }
+
+                        @Override
+                        public Priority fromString(String s) {
+                            switch (s) {
+                                case "URGENT" : return Priority.URGENT;
+                                case "NORMAL" : return Priority.NORMAL;
+                                case "SECONDAIRE" : return Priority.SECONDARY;
+                            };
+                            throw new IllegalArgumentException("Not a valid string to receive");
+                        }
+                    }, FXCollections.observableArrayList(Priority.values())
+            );
+            tc.setComboBoxEditable(true);
+            return tc;
+        });
+        treeView.getColumns().add(column);
+    }
+
 }
